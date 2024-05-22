42d80cc07dbb8de4b8ba2178aecbb6c864f8935e
Banyc <36535895+Banyc@users.noreply.github.com>
2024-05-22 22:39:02 +0800

    feat: read and write df files

---
+++ src/lib.rs
@@ -0,0 +1,63 @@
+use std::path::Path;
+
+use polars::prelude::*;
+
+pub fn read_df_file(
+    path: impl AsRef<Path>,
+    infer_schema_length: Option<usize>,
+) -> anyhow::Result<LazyFrame> {
+    let Some(extension) = path.as_ref().extension() else {
+        anyhow::bail!(
+            "No extension at the name of the file `{}`",
+            path.as_ref().to_string_lossy()
+        );
+    };
+    Ok(match extension.to_string_lossy().as_ref() {
+        "csv" => LazyCsvReader::new(&path)
+            .with_has_header(true)
+            .with_infer_schema_length(infer_schema_length)
+            .finish()?,
+        "json" => {
+            let file = std::fs::File::options().read(true).open(&path)?;
+            JsonReader::new(file).finish()?.lazy()
+        }
+        "ndjson" | "jsonl" => LazyJsonLineReader::new(&path)
+            .with_infer_schema_length(infer_schema_length)
+            .finish()?,
+        _ => anyhow::bail!(
+            "Unknown extension `{}` at the name of the file `{}`",
+            extension.to_string_lossy(),
+            path.as_ref().to_string_lossy()
+        ),
+    })
+}
+
+pub fn write_df_output(mut df: DataFrame, path: impl AsRef<Path>) -> anyhow::Result<()> {
+    let Some(extension) = path.as_ref().extension() else {
+        anyhow::bail!(
+            "No extension at the name of the file `{}`",
+            path.as_ref().to_string_lossy()
+        );
+    };
+    let output = std::fs::File::options()
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .open(&path)?;
+    match extension.to_string_lossy().as_ref() {
+        "csv" => CsvWriter::new(output).finish(&mut df)?,
+        "json" => JsonWriter::new(output).finish(&mut df)?,
+        "ndjson" | "jsonl" => {
+            anyhow::bail!(
+                "No `JsonLineWriter` available to write `{}`",
+                path.as_ref().to_string_lossy()
+            );
+        }
+        _ => anyhow::bail!(
+            "Unknown extension `{}` at the name of the file `{}`",
+            extension.to_string_lossy(),
+            path.as_ref().to_string_lossy()
+        ),
+    }
+    Ok(())
+}
